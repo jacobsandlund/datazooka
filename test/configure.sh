946759bdd0f0a603ae003d13c5294991d142a0d0
Jake Sandlund <jtsandlund@gmail.com>
2012-09-20 00:11:46 -0500

    Change test html to use /lib.

--- test/configure.sh
+++ test/configure.sh
@@ -7,16 +7,16 @@ start() {
 }
 
 start Combined | sed '/<\/body>/i\
-  <script src="crossfilter.v1.js"></script>\
-  <script src="d3.v2.js"></script>\
-  <script src="../binfo.js"></script>\
+  <script src="/lib/crossfilter.js"></script>\
+  <script src="/lib/d3.js"></script>\
+  <script src="/binfo.js"></script>\
   <script src="test.js"></script>\
   ' > test/combined.html
 
 start Loader | sed '/<\/body>/i\
-  <script src="crossfilter.v1.js"></script>\
-  <script src="d3.v2.js"></script>\
-  <script src="../vaccine_loader.js"></script>\
+  <script src="/lib/crossfilter.js"></script>\
+  <script src="/lib/d3.js"></script>\
+  <script src="/vaccine_loader.js"></script>\
   <script>vaccine_load("/test/test.js");</script>\
   ' > test/loader.html
 
