b2e85c47042961fa5642005c5b30dcc75f4c1f40
Jake Sandlund <jtsandlund@gmail.com>
2012-09-23 14:05:04 -0500

    Remove definitionsFromJSON.

--- src/index.js
+++ src/index.js
@@ -16,41 +16,6 @@ define('datazooka/index', function(require, exports) {
   exports.setup = core.setup;
   exports.defaultRender = core.defaultRender;
 
-  exports.definitionsFromJSON = function(dataName, defns) {
-    /*jshint evil:true */
-    var id, defn,
-        evil = [],
-        evalParts = ['dimension', 'group', 'round', 'x', 'y', 'format'],
-        evalPartsIfFunc = ['type', 'ordinal'];
-    function makeEvil(defn, id) {
-      return function(part) {
-        if (!defn[part]) {
-          return;
-        }
-        evil.push('defns["', id, '"].', part, ' = ', defn[part], ';');
-      };
-    }
-    function maybeMakeEvil(defn, id) {
-      var evalPart = makeEvil(defn, id);
-      return function(part) {
-        if (typeof defn[part] === 'string' &&
-            defn[part].slice(0, 8) === 'function') {
-          evalPart(part);
-        }
-      };
-    }
-
-    for (id in defns) {
-      if (defns.hasOwnProperty(id)) {
-        defn = defns[id];
-        evalParts.forEach(makeEvil(defn, id));
-        evalPartsIfFunc.forEach(maybeMakeEvil(defn, id));
-      }
-    }
-    eval(evil.join(''));
-    exports.definitions(dataName, defns);
-  };
-
   exports.definitions = function(dataName, defns) {
     var id;
     for (id in defns) {
--- test/test.js
+++ test/test.js
@@ -3,27 +3,57 @@ datazooka.setup({
   root: document,
   holder: '#charts'
 });
-datazooka.defaultRender('flights', ['time-delay', 'distance'], {
-  filter: {distance: [200, 600]},
-  given: {'time-delay': 'yc'}
+
+datazooka.defaultRender('flights', ['time', 'delay', 'delay-time', 'time-day', 'day'], {
+  filter: {'delay': [20, 150]}, given: {'delay-time': 'xc'}, filterLevels: {'delay-time': [34, 76]}
 });
 
-// Randomly get the definitions before or after the data comes in.
-var before = Math.random() < 0.5
-console.log(before ? 'definitions first' : 'data first');
-
-function definitions() {
-  d3.json('test.json', function(jsonDefinitions) {
-    datazooka.definitionsFromJSON('flights', jsonDefinitions);
-  });
-}
-
-if (before) definitions();
+datazooka.definitions('flights', {
+  time: {
+    label: "Time of Day",
+    derived: true,
+    dimension: function(d) { return d.date.getHours() + d.date.getMinutes() / 60; }
+  },
+  delay: {
+    label: "Arrival Delay (min.)",
+    dimension: function(d) { return Math.max(-60, Math.min(149, d.delay)); }
+  },
+  distance: {
+    label: "Distance (mi.)",
+    dimension: function(d) { return Math.min(1999, d.distance); }
+  },
+  date: {
+    label: "Date",
+    type: function(d) { return new Date(2001, d.slice(0, 2) - 1, d.slice(2, 4), d.slice(4, 6), d.slice(6, 8)); },
+    dimension: function(d) { return d3.time.day(d.date); },
+    round: d3.time.day.round,
+    groupIdentity: true,
+    separation: 86400000,
+    tickSpacing: 120,
+    x: d3.time.scale().domain([new Date(2001, 0, 1), new Date(2001, 3, 1)]).range([0, 990])
+  },
+  day: {
+    label: "Day of the Week",
+    derived: true,
+    dimension: function(d) { var day = d.date.getDay() - 1; if (day < 0) { return 6; } else { return day; } },
+    ordinal: function(d) { return d; },
+    format: function(d) { return ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'][d]; }
+  },
+  origin: {
+    label: "Origin Airport",
+    dimension: function(d) { return d.origin; },
+    ordinal: function(d) { return d; },
+  },
+  destination: {
+    label: "Destination Airport",
+    dimension: function(d) { return d.destination; },
+    ordinal: function(d) { return d; },
+  },
+});
 
 // (It's CSV, but GitHub Pages only gzip's JSON at the moment.)
 d3.csv("flights-3m.json", function(flights) {
   datazooka.dataFromUntyped('flights', flights);
-  if (!before) definitions();
 });
 
 datazooka.definitions('compare-test', {
@@ -34,11 +64,12 @@ datazooka.definitions('compare-test', {
 var data = [],
     i,
     j,
-    k;
+    k,
+    compareLevels = 100;  // copied from config
 
-for (i = 0; i < datazooka.compareLevels; i++) {
+for (i = 0; i < compareLevels; i++) {
   for (j = 0; j < i; j++) {
-    for (k = 0; k < datazooka.compareLevels; k++) {
+    for (k = 0; k < compareLevels; k++) {
       data.push({x: i, y: k});
     }
   }
