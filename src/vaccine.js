04342a6f4334246d10a785bcabce26d4865d33d7
Jake Sandlund <jtsandlund@gmail.com>
2012-10-13 16:44:51 -0500

    Simple vaccine with d3 and crossfilter deps.

--- src/vaccine.js
+++ src/vaccine.js
@@ -1,63 +1,46 @@
-
 function define(id, defn) {
 
-  var globalVaccine =
-  window.vaccine || (window.vaccine = {
-    // The minimal code required to be vaccine compliant.
 
-    // w = waiting: Functions to be called when a modules
-    // gets defined. w[moduleId] = [array of functions];
-    w: {},
-
-    // m = modules: Modules that have been fully defined.
-    // m[moduleId] = module.exports value
-    m: {},
-
-    // s = set: When a module becomes fully defined, set
-    // the module.exports value here.
-    // s(moduleId, module.exports)
-    s: function(id, val) {
-      this.m[id] = val;
-      (this.w[id] || []).forEach(function(w) { w(); });
-    }
-  });
-  // Set your library with vaccine.s('mylib', mylib);
-
-  var parts = id.split('/');
 
   var module = {exports: {}};
 
   function require(reqId) {
-
-    var matching = /(\.?\.\/?)*/.exec(reqId)[0],
-        // Some code golf to get the number of "directories" back we want to go
-        back = Math.floor(matching.replace(/\//g, '').length / 1.9 + 0.99),
-        base;
-    if (back) {
-      base = parts.slice(0, parts.length - back).join('/');
-      if (base) base += '/';
-      reqId = base + reqId.slice(matching.length);
-    }
-    reqId = reqId.replace(/\/$/, '');
-    var mod = globalVaccine.m[reqId];
-    if (!mod) {
-      require.id = reqId;
-      throw require;  // Throw require, to ensure correct error gets handled
-    }
-
-    return mod;
+    return globalVaccine.m[reqId.replace('.', 'datazooka')];
   }
 
-  try {
+
     defn(require, module.exports, module);
     globalVaccine.s(id, module.exports);
-    if (id.match(/\/index$/)) {
-      globalVaccine.s(id.replace(/\/index$/,''), module.exports);
-    }
-  } catch (e) {
-    if (e != require) throw e;
-    (globalVaccine.w[require.id] || (globalVaccine.w[require.id] = []))
-        .push(function() { define(id, defn); });
-  }
 }
 
+var
+    vaccineDependencies = ["d3","crossfilter"],
+    vaccineRemainingDeps = vaccineDependencies.length,
+    vaccineRunDefines = function() { --vaccineRemainingDeps || vaccineDefines(); },
+    globalVaccine =
+window.vaccine || (window.vaccine = {
+  // The minimal code required to be vaccine compliant.
+
+  // w = waiting: Functions to be called when a modules
+  // gets defined. w[moduleId] = [array of functions];
+  w: {},
+
+  // m = modules: Modules that have been fully defined.
+  // m[moduleId] = module.exports value
+  m: {},
+
+  // s = set: When a module becomes fully defined, set
+  // the module.exports value here.
+  // s(moduleId, module.exports)
+  s: function(id, val) {
+    this.m[id] = val;
+    (this.w[id] || []).forEach(function(w) { w(); });
+  }
+});
+vaccineDependencies.forEach(function(d) {
+  if (globalVaccine.m[d]) --vaccineRemainingDeps;
+  else  (globalVaccine.w[d] ||
+          (globalVaccine.w[d] = [])
+        ).push(vaccineRunDefines);
+});
+if (!vaccineRemainingDeps) vaccineDefines();
