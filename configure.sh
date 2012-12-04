bd91e5a3ba213e744897422243f5635dc5258dfa
Jake Sandlund <jtsandlund@gmail.com>
2012-12-04 08:24:30 -0600

    Upload assets to S3 with asset_store.

--- configure.sh
+++ configure.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 development="public/test.html"
-production="public/index.html"
 demo="flights"
 sed -e '/<!-- DEVELOPMENT -->/,/<!-- \/DEVELOPMENT -->/d' \
     -e 's#<!-- PRODUCTION -->#<script src="/datazooka.js"></script>#' \
-    -e 's#\(/[^/]*\)\.js"#\1.min.js"#g' -e 's#\(/[^/]*\)\.css"#\1.min.css"#g' \
     -e "s/test.js/\/demos\/$demo.js/" \
-    $development > $production
+    $development
--- public/test.js
+++ public/test.js
@@ -124,20 +124,20 @@ datazooka.setup();
 
 datazooka.definitions('flights', {
   time: {
-    label: "Time of Day",
+    label: 'Time of Day',
     derived: true,
     dimension: function(d) { return d.date.getHours() + d.date.getMinutes() / 60; }
   },
   delay: {
-    label: "Arrival Delay (min.)",
+    label: 'Arrival Delay (min.)',
     dimension: function(d) { return Math.max(-60, Math.min(149, d.delay)); }
   },
   distance: {
-    label: "Distance (mi.)",
+    label: 'Distance (mi.)',
     dimension: function(d) { return Math.min(1999, d.distance); }
   },
   date: {
-    label: "Date",
+    label: 'Date',
     type: function(d) { return new Date(2001, d.slice(0, 2) - 1, d.slice(2, 4), d.slice(4, 6), d.slice(6, 8)); },
     dimension: function(d) { return d3.time.day(d.date); },
     round: d3.time.day.round,
@@ -147,25 +147,25 @@ datazooka.definitions('flights', {
     x: d3.time.scale().domain([new Date(2001, 0, 1), new Date(2001, 3, 1)]).range([0, 990])
   },
   day: {
-    label: "Day of the Week",
+    label: 'Day of the Week',
     derived: true,
     dimension: function(d) { var day = d.date.getDay() - 1; if (day < 0) { return 6; } else { return day; } },
     ordinal: function(d) { return d; },
     format: function(d) { return ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'][d]; }
   },
   origin: {
-    label: "Origin Airport",
+    label: 'Origin Airport',
     dimension: function(d) { return d.origin; },
     ordinal: function(d) { return d; },
   },
   destination: {
-    label: "Destination Airport",
+    label: 'Destination Airport',
     dimension: function(d) { return d.destination; },
     ordinal: function(d) { return d; },
   },
 });
 
-d3.csv("/demos/flights.json", function(flights) {
+d3.csv('/demos/flights.csv', function(flights) {
   datazooka.dataFromUntyped('flights', flights);
 });
 
