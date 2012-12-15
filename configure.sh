fcdad1062852917e584ab585af3820530e72e6ed
Jake Sandlund <jtsandlund@gmail.com>
2012-12-15 16:44:24 -0600

    Make build file do html modifications.

--- configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-development="public/test.html"
-demo="flights"
-sed -e '/<!-- DEVELOPMENT -->/,/<!-- \/DEVELOPMENT -->/d' \
-    -e 's#<!-- PRODUCTION -->#<script src="/datazooka.js"></script>#' \
-    -e "s/test.js/\/demos\/$demo.js/" \
-    $development
